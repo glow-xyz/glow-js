2beac17661773284e04fab4174e97141d2444810
Victor Pontis <victor@pont.is>
2022-06-05 23:44:44 +0200

    Update solana-client build process (#8)

---
+++ packages/solana-client/src/borsh/index.ts
@@ -0,0 +1,2 @@
+export * from "./GlowBorshTypes";
+export * from "./base";
--- packages/solana-client/src/index.ts
+++ packages/solana-client/src/index.ts
@@ -1,3 +1,7 @@
 export { GTransaction } from "./GTransaction";
 export { GPublicKey } from "./GPublicKey";
-export { SolanaClient } from "./client/solana-client";
+export * from "./client/solana-client";
+export * from "./client/client-types";
+export * from "./client/error-codes";
+export * from "./client/rpc-types";
+export * from "./borsh/index";
