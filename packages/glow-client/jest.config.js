6a0a573b98923f321d18619348d4944ed1a1817c
Victor Pontis <victor@pont.is>
2022-06-11 11:49:49 -0400

    Add test

---
+++ packages/glow-client/jest.config.js
@@ -0,0 +1,20 @@
+// Inspired by https://github.com/arcatdmz/nextjs-with-jest-typescript/blob/master/jest.config.js
+module.exports = {
+  preset: "ts-jest/presets/js-with-ts",
+  testEnvironment: "node",
+  moduleFileExtensions: ["ts", "tsx", "js"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  testMatch: ["**/__tests__/*.test.(ts|tsx)"],
+  testPathIgnorePatterns: ["./dist", "./node_modules/"],
+  collectCoverage: false,
+  globals: {
+    "ts-jest": {
+      // This helps the tests run faster
+      // But it skips typechecking, so that should be a different step on CI
+      // https://huafu.github.io/ts-jest/user/config/isolatedModules
+      isolatedModules: true,
+    },
+  },
+};
