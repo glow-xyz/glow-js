ff40ce4abca47454bc1445c4ea9b88bbf04208d8
Stanisław Chmiela <sjchmiela@gmail.com>
2022-09-08 22:17:19 +0200

    Upgrade `ts-jest`

--- packages/glow-client/jest.config.js
+++ packages/glow-client/jest.config.js
@@ -4,17 +4,17 @@ module.exports = {
   testEnvironment: "node",
   moduleFileExtensions: ["ts", "tsx", "js"],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        // This helps the tests run faster
+        // But it skips typechecking, so that should be a different step on CI
+        // https://huafu.github.io/ts-jest/user/config/isolatedModules
+        isolatedModules: true,
+      },
+    ],
   },
   testMatch: ["**/__tests__/*.test.(ts|tsx)"],
   testPathIgnorePatterns: ["./dist", "./node_modules/"],
   collectCoverage: false,
-  globals: {
-    "ts-jest": {
-      // This helps the tests run faster
-      // But it skips typechecking, so that should be a different step on CI
-      // https://huafu.github.io/ts-jest/user/config/isolatedModules
-      isolatedModules: true,
-    },
-  },
 };
