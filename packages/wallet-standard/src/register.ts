2d7056741f8de400b98574cc31d335df80d84898
Jordan Sexton <jordan@jordansexton.com>
2022-10-19 11:47:21 -0500

    pass glow adapter to standard wallet

--- packages/wallet-standard/src/register.ts
+++ packages/wallet-standard/src/register.ts
@@ -1,12 +1,13 @@
+import type { GlowAdapter } from "@glow-xyz/glow-client";
 import type { WalletsWindow } from "@wallet-standard/base";
 import { GlowWallet } from "./wallet.js";
 
 declare const window: WalletsWindow;
 
-export function register(): void {
+export function register(glow: GlowAdapter): void {
   try {
     (window.navigator.wallets ||= []).push(({ register }) =>
-      register(new GlowWallet())
+      register(new GlowWallet(glow))
     );
   } catch (error) {
     console.error(error);
